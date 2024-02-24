f6c22a8cf5533bc0605558ecd26e8703b05f6a1d
Daryl Malibiran <33365949+djmalibiran@users.noreply.github.com>
2024-02-24 17:12:03 +0800

    Upgrade to v0.1.1

--- button-text-only-style.php
+++ button-text-only-style.php
@@ -4,7 +4,7 @@
  * Description:       "Button Text Only Style" is a lightweight WordPress plugin designed to enhance the styling options for core buttons within WordPress. This plugin enables the addition of custom text-only styles to core buttons, allowing users to easily customize the appearance of buttons on their WordPress websites without the need for extensive coding or styling knowledge. With simplicity at its core, this plugin provides a straightforward solution for users seeking to refine the visual presentation of buttons on their WordPress sites.
  * Requires at least: 6.1
  * Requires PHP:      7.0
- * Version:           0.1.0
+ * Version:           0.1.1
  * Author:            Daryl Malibiran
  * License:           GPL-2.0-or-later
  * License URI:       https://www.gnu.org/licenses/gpl-2.0.html
